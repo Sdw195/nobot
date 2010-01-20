3634e877bdd91f3d2767649aba3fc68cf71cf561
alterecco <alterecco@gmail.com>
2010-01-20 17:18:55 +0000

    Added simple trac plugin

---
+++ modules/trac.py
@@ -0,0 +1,40 @@
+from lib.bot import command
+from lib.tools import Database
+
+class TracDB(Database):
+
+    def create_structure(self):
+        c = self.con.cursor()
+        c.execute("""
+            CREATE TABLE trac (
+                  id INTEGER PRIMARY KEY,
+                  ticket INTEGER UNIQUE,
+                  summary TEXT,
+                  component TEXT,
+                  version TEXT,
+                  milestone TEXT,
+                  type TEXT,
+                  owner TEXT,
+                  status TEXT,
+                  created TIMESTAMP,
+                  updated TIMESTAMP,
+                  description TEXT,
+                  reporter TEXT
+            )""")
+        self.con.commit()
+
+class TicketId(command):
+
+    regex = r".*(?:%(cmd)s(?=\s+\d+)|t#(?=\d+))\s*(\d+).*"
+    triggers = [r"t#\d+"]
+    syntax = 'ticketid id | t#id'
+    example = "Please see t#320"
+    doc = " ".join(
+        [ "Print an url to the id of the ticket specified"
+        ])
+
+    def run(self, bot, data):
+        tid = data.group(1)
+        if tid:
+            tracker = "http://wiki.neurohack.com/transcendence/trac/ticket/%s" % tid
+            bot.say("Tracker ticket #%s: %s" % (tid, tracker))
