55be3b7593f0201158200acff3421be7c0b1c699
sdw 195 <sdw195@gmail.com>
2011-03-03 15:10:45 +1300

    removed unneeded lines

--- modules/sandwich.py
+++ modules/sandwich.py
@@ -8,22 +8,16 @@ class sandwich(command):
  
     rule = r"\s+(.*)"
  
-    syntax = ""
+    syntax = "sandwich [receiver]"
     doc = ""
  
     def run(self, bot, data):
-	
-
- 
  
         topping = random.choice(['avocado','lettuce','tomato','mozzarella cheese','bacon','spam','peanutbutter','bratwurst','cruelty-free PeTA-approved fake guinea-pig','digital','cucumber','tofu','-if slightly burnt-','recursive','banana','ice-cream','ham&jam','tuna','double cheese','olive','..erm.. just','self-made','generic','sand','witch','two-hander'])
  
         ## lets rename `a' to something more legible
         receiver = data.group(1)
- 
-        ## just because we are curious, lets us see who receiver is
-        ##bot.say("Receiver is %s" % receiver)
- 
+
         if receiver == "Sdw195": 
             topping = 'BBBBBBLT'
         if receiver == "sheepluva":
@@ -31,5 +25,5 @@ class sandwich(command):
  
         ## instead of using data.group(1) here again, we might as well use `receiver'
         text = '\x01ACTION gives ' + receiver + ' a ' + topping + ' sandwich\x01'
- 	##bot.say("\x01ACTION gives %s a %s\x01" % (receiver, topping))
+ 	
         bot.say(text.strip())
